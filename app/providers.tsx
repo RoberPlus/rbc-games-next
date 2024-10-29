21b01d30db2f2c7f5619c94139ad254148983ff6
Robert C <carderober@gmail.com>
2024-10-29 12:35:53 -0300

    Auth provider - context - hook created

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,23 @@
 'use client';
 
+import { AuthProvider } from '@/contexts/auth-provider';
 import ThemeProvider from '../contexts/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Toaster />
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        {children}
-      </ThemeProvider>
+      <AuthProvider>
+        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </AuthProvider>
     </>
   );
 };
